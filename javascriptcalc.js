1e38a2fb57f2d9c80849aafbf536d5cd37771801
NisargNayak17 <128769461+NisargNayak17@users.noreply.github.com>
2023-09-11 13:13:01 -0400

    Add files via upload

---
+++ javascriptcalc.js
@@ -0,0 +1,12 @@
+/**let displayField = document.getElementById("display-field");
+let buttons = document.querySelectorAll(".btn");
+
+buttons.forEach(button => {
+    button.addEventListener("click",(event) => {
+        event.preventDefault();
+        var buttonsValue = button.value;
+        displayField.value = displayField.value + buttonsValue;
+    });
+});**/
+let string = "";
+let button = "";
